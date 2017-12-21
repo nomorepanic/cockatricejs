321fc4b48497bb1499652d7aef26dcbbaa719d00
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-12-21 21:47:26 +0100

    feat(cli): add compile method

--- cast/Cli.js
+++ cast/Cli.js
@@ -4,6 +4,16 @@ const App = require('./App');
 
 
 class Cli {
+    static compile() {
+        Commander
+            .command('compile <filesDirectory> <templatePath>')
+            .action((filesDirectory, templatePath) => {
+                const app = new App();
+                app.findFiles(filesDirectory);
+                app.makePages(templatePath);
+            });
+    }
+
     static main() {
         Cli.compile();
         Commander.parse(process.argv);
--- tests/Cli.js
+++ tests/Cli.js
@@ -5,6 +5,16 @@ const Cli = require('../cast/Cli');
 
 
 describe('the Cli module', () => {
+    it('should have a compile method', () => {
+        const command = Td.object();
+        Td.replace(Commander, 'command');
+        Td
+            .when(Commander.command('compile <filesDirectory> <templatePath>'))
+            .thenReturn(command);
+        Cli.compile();
+        Td.verify(command.action(Td.matchers.anything()));
+    });
+
     it('should have a main method', () => {
         Td.replace(Commander, 'parse');
         Td.replace(Cli, 'compile');
